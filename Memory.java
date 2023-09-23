e90bb0cc14b25ebb48eaae62002d04c1b22eb296
MrHonor <81781425+MrHonorCodes@users.noreply.github.com>
2023-09-22 21:32:34 -0500

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -1,145 +1,74 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- *
- * Devang Mistry
- * Memory class will read the instructions from the file, initialize the 
- * memory array and communicate with the CPU
- */
+public class MemUnit {
+    private static final int[] storage = new int[2000];
 
-public class Memory
-{   
-    final static int [] memory = new int[2000]; // int array to implement memory
-    
-    public static void main(String args[])
-    {
-        try
-        {
-            //Create a File object and a scanner to read it
-            Scanner CPU_reader = new Scanner(System.in);
-            File file = null;
-            if(CPU_reader.hasNextLine())    // read file name from CPU
-            {
-                file = new File(CPU_reader.nextLine());
-                
-                if(!file.exists()) //exit if file not found
-                {
-                    System.out.println("File not found");
-                    System.exit(0);
-                }
+    public static void main(String[] args) {
+        Scanner cpuInput = new Scanner(System.in);
+        File instructionFile = null;
+
+        if (cpuInput.hasNextLine()) {
+            instructionFile = new File(cpuInput.nextLine());
+            if (!instructionFile.exists()) {
+                System.out.println("Oops, file's gone missing!");
+                System.exit(0);
             }
-            
-            // call function to read file and initialize memory array
-            readFile(file);
+        }
 
-            String line;
-            int temp2;
-            /*
-                This while loop will keep on reading instructions from the CPU process
-                and perform the read or write function appropriately
-            */
-            while(true)
-            {
-                if(CPU_reader.hasNext())
-                {
-                    line = CPU_reader.nextLine(); //read the comma delimited line sent by the CPU
-                    if(!line.isEmpty())
-                    {
-                        String [] j = line.split(","); //split the line to get the necessary tokens
-                        
-                        //  if first token is 1 then CPU is requesting to read 
-                        //  from an address
-                        if(j[0].equals("1"))    
-                        {
-                            temp2 = Integer.parseInt(j[1]);
-                            System.out.println(read(temp2));// send requested data to CPU 
-                        }
-                        
-                        //  else it will be 2, which means CPu is requesting to 
-                        //  write data at a particular address
-                        else
-                        {
-                            int i1 = Integer.parseInt(j[1]);
-                            int i2 = Integer.parseInt(j[2]);
-                            write(i1,i2); // invoke the write function
-                        }
+        initMemory(instructionFile);
+
+        while (true) {
+            if (cpuInput.hasNext()) {
+                String commandLine = cpuInput.nextLine();
+                if (!commandLine.trim().isEmpty()) {
+                    String[] tokens = commandLine.split(",");
+                    if ("1".equals(tokens[0])) {
+                        int addr = Integer.parseInt(tokens[1]);
+                        System.out.println(fetch(addr));
+                    } else {
+                        int addr = Integer.parseInt(tokens[1]);
+                        int data = Integer.parseInt(tokens[2]);
+                        store(addr, data);
                     }
-                    else 
-                        break;
-                }
-                else
+                } else {
                     break;
+                }
+            } else {
+                break;
             }
-            
-        } catch(NumberFormatException e)
-        {
+        }
+    }
+
+    private static int fetch(int address) {
+        return storage[address];
+    }
+
+    private static void store(int address, int value) {
+        storage[address] = value;
+    }
+
+    private static void initMemory(File file) {
+        try {
+            Scanner fileScanner = new Scanner(file);
+            int index = 0;
+            while (fileScanner.hasNext()) {
+                if (fileScanner.hasNextInt()) {
+                    storage[index++] = fileScanner.nextInt();
+                } else {
+                    String token = fileScanner.next();
+                    if (token.startsWith(".")) {
+                        index = Integer.parseInt(token.substring(1));
+                    } else if ("//".equals(token)) {
+                        fileScanner.nextLine();
+                    } else {
+                        fileScanner.nextLine();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
     }
-    
-    // function to read the data in the address provided by the CPU
-    public static int read(int address)
-    {
-        return memory[address];
-    }
-    
-    // function to write data into an address based on intruction given by CPU
-    public static void write(int address, int data)
-    {
-        memory[address] = data;
-    }
-
-    // function to read instructions from file and initialize memory
-    private static void readFile(File file) {
-        
-        try 
-        {
-            Scanner scanner = new Scanner(file);
-            String temp;
-            int temp_int;
-            int i = 0;
-
-            /*
-            *   Read the file to load memory instructions
-            */
-            while(scanner.hasNext())
-            {
-                //if integer found then write to memory array
-                if(scanner.hasNextInt())
-                {
-                    temp_int = scanner.nextInt();
-                    memory[i++] = temp_int;
-                }
-                else
-                {
-                    temp = scanner.next();
-                    // if token starts with ".", then move the counter appropriately
-                    if(temp.charAt(0) == '.')
-                    {
-                        i = Integer.parseInt(temp.substring(1));
-                    }
-                    
-                    // else if the token is a comment then skip the line
-                    else if(temp.equals("//"))
-                    {
-                        scanner.nextLine();
-                    }
-                    
-                    // skip the line if anything else
-                    else
-                        scanner.nextLine();
-                }
-            }
-        } 
-        catch (FileNotFoundException ex) 
-        {
-            ex.printStackTrace();
-        }
-        
-    }
-
 }
